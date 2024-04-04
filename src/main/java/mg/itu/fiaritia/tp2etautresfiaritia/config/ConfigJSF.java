b10ec25fb0866c7c2d8d6633f150f47c686a7407
Fiaritia <fiaritia-naliarilala.rahamison@etu.unice.fr>
2024-04-04 23:29:10 +0300

    Analyse d'un post tout simple: création des fichiers et configs + analyse.

---
+++ src/main/java/mg/itu/fiaritia/tp2etautresfiaritia/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.fiaritia.tp2etautresfiaritia.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ * Config pour activer JSF
+ * @author raham
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
