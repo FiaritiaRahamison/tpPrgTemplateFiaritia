9f3465f2f1de98ab0329130c3015b5b8d80accb4
Fiaritia <70196731+Fiaritia@users.noreply.github.com>
2024-04-06 21:24:32 +0300

    Utilisation des paramètres de vue: le nombre précédemment inséré (qu'on retrouve dans l'url) est dans l'input d'insertion par défaut.

---
+++ src/main/java/mg/itu/fiaritia/tp2etautresfiaritia/jsf/Bean4.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.fiaritia.tp2etautresfiaritia.jsf;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author raham
+ */
+@Named(value = "bean4")
+@RequestScoped
+public class Bean4 {
+
+    private int nombre;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+
+    /**
+     * Afficher le résultat correcte en prenant en compte le nombre saisi par l'user.
+     * @return String
+     */
+    public String afficher() {
+        return "affichage_4?nb="+nombre+"&faces-redirect=true";
+    }
+    
+    /**
+     * Creates a new instance of Bean
+     */
+    public Bean4() {
+    }
+
+}
